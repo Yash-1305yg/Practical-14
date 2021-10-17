efde3cf48caab0a4a5624e73d92ce9ff8af9535a
Yash-1305yg <yash.gupta_cs19@gla.ac.in>
2021-10-17 11:59:59 +0530

    second commit

---
+++ javas.js
@@ -0,0 +1,30 @@
+function increase()
+{
+    var a = document.getElementById('text').textContent;
+    var b =parseInt(a);
+    b=b+1;
+    var c=b.toString();
+    document.getElementById('text').textContent = c; 
+    document.getElementById('input1').style.borderColor="black";
+}
+
+function decrease()
+{
+    var a = document.getElementById('text').textContent;
+    var b =parseInt(a);
+    if(b==1)
+    {
+        document.getElementById('input1').style.borderColor="yellow";
+    }
+    if(b==0)
+    {
+        document.getElementById('input1').style.disabled=true;
+    }
+    else
+    {
+        b=b-1;
+        var c=b.toString();
+        document.getElementById('text').textContent = c;
+     }
+   
+}
